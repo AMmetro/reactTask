8df3823241c02cf0132bba4d9c812bf21c01335c
AMmetro <metro.trend.now@gmail.com>
2021-01-27 22:02:18 +0300

    Updates

---
+++ precache-manifest.73cd6fb8e08df34c519f74a6ba5bf3b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38059046f33ee1a8d07ff0439b21f50",
+    "url": "/reactTask/index.html"
+  },
+  {
+    "revision": "f96ce0f01c83c6f41e29",
+    "url": "/reactTask/static/css/main.4553041b.chunk.css"
+  },
+  {
+    "revision": "78bd6108cb8a7468782f",
+    "url": "/reactTask/static/js/2.8d1adf60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactTask/static/js/2.8d1adf60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96ce0f01c83c6f41e29",
+    "url": "/reactTask/static/js/main.1da3d915.chunk.js"
+  },
+  {
+    "revision": "018fa99116a09510a82b",
+    "url": "/reactTask/static/js/runtime-main.cf50bb16.js"
+  }
+]);
