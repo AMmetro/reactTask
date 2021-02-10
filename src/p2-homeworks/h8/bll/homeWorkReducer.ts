9138c3007c5a91bd2155e1301142a2d4adbf2a8e
AMmetro <metro.trend.now@gmail.com>
2021-02-10 22:10:26 +0300

    task 7 deploy-3

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,7 +1,13 @@
 export const homeWorkReducer = (state: any, action: any): any => {
     switch (action.type) {
         case "sort": {
-            console.log(action)
+
+            // let newState=state.sort(function (a:any,b:any){return a-b})
+            let newState=state.sort(function (a:any,b:any){if(a.name < b.name) return -1;
+                if(a.name > b.name) return 1;})
+
+            console.log("newState=")
+            console.log(newState)
             return state
         }
         case "check": {
