6e0e4182985e0a8738d2643c8cc6d5bc46cc12d7
AMmetro <metro.trend.now@gmail.com>
2021-02-11 00:28:54 +0300

    Updates

---
+++ precache-manifest.1cb4e3648e45ae681fd38e497f6fbcb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2d2060b175ce1746520d54e2fc3d3c",
+    "url": "/reactTask/index.html"
+  },
+  {
+    "revision": "382d04e6d6afb2512e74",
+    "url": "/reactTask/static/css/main.bae58852.chunk.css"
+  },
+  {
+    "revision": "f76f73d5e9a99ea58104",
+    "url": "/reactTask/static/js/2.5644d540.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactTask/static/js/2.5644d540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382d04e6d6afb2512e74",
+    "url": "/reactTask/static/js/main.22501e23.chunk.js"
+  },
+  {
+    "revision": "b3a0488300ee48f184e6",
+    "url": "/reactTask/static/js/runtime-main.1f039de0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactTask/precache-manifest.f28764e88c5ae6b7b4707f2cbf7ab658.js"
+  "/reactTask/precache-manifest.1cb4e3648e45ae681fd38e497f6fbcb2.js"
 );
 
 self.addEventListener('message', (event) => {
