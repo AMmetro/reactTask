27687d88f054b5d46818f9e4b3d28adb825bb6f6
AMmetro <metro.trend.now@gmail.com>
2021-02-11 21:20:55 +0300

    nexe fix

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -15,6 +15,7 @@ import HW9 from "../../../p2-homeworks/h9/HW9";
 function App() {
     return (
         <div className={s.App}>
+            <p></p>
             {/*<HW1/>*/}
             {/*<HW2/>*/}
             {/*<HW3/>*/}
